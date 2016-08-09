c4cebacc54b3833caeb2042f049f260a5320e317
Paulo Henrique Cuchi <paulo.cuchi@gmail.com>
2016-08-08 21:35:39 -0300

    some improvements

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 let app = require('express')()
+let logger = require('morgan');
+
+app.use(logger('dev'));
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.status(200).send('Hello world!');
+});
+
+// Did not match any method, show a 404 error...
+app.use((req, res, next) => {
+    res.status(404).send('Page not found!');
 });
 
 app.listen(8000, () => {
