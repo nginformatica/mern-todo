eb4dfd7d3e36a6a1df96699e9a2c2ddaf49be301
Paulo Henrique Cuchi <paulo.cuchi@gmail.com>
2016-08-12 09:09:50 -0300

    user model

---
+++ server/model/user.js
@@ -0,0 +1,18 @@
+import mongodb from 'mongoose';
+
+export default mongodb.model('User', new mongodb.Schema({
+  name: {
+    type: String,
+    required: [true, 'User name is required!']
+  },
+  email: {
+    type: String,
+    required: [true, 'E-mail is required!']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required!']
+    minlength: [4, 'Password is too short!']
+  },
+  created: Date
+});
