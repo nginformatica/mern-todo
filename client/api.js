6dc95480d6281d8e7023682ad972977765df3d84
Paulo Henrique Cuchi <paulo.cuchi@gmail.com>
2016-08-31 18:04:10 -0300

    starting with redux in the client

---
+++ client/api.js
@@ -0,0 +1,23 @@
+
+export default function call(endpoint, method = 'GET', body) {
+    return fetch('api/' + endpoint, {
+        headers: { 'Content-Type': 'application/json' },
+        method,
+        body: JSON.stringify(body)
+    })
+    .then(response => {
+        return {
+            body: response.json().catch(() => response.text()),
+            response
+        };
+    })
+    .then(({ body, response }) => {
+        return response.ok
+            ? body
+            : Promise.reject(body);
+    })
+    .then(
+        body => body,
+        error => error
+    );
+}
